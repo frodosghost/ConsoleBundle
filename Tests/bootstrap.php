afff7710be2969370536805d25a4bf608c1b4cdd
James Rickard <james@frodosghost.com>
2012-03-19 15:24:51 +1100

    Updated bootstrap file to work. Was copied from an older bundle and code updated to work.

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/HttpFoundation/UniversalClassLoader.php';
+require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
-use Symfony\Component\HttpFoundation\UniversalClassLoader;
+use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
@@ -10,7 +10,7 @@ $loader->register();
 
 spl_autoload_register(function($class)
 {
-    if (0 === strpos($class, 'Bundle\\Kris\\TwitterBundle\\')) {
+    if (0 === strpos($class, 'Manhattan\\Bundle\\ConsoleBundle\\')) {
         $path = implode('/', array_slice(explode('\\', $class), 3)).'.php';
         require_once __DIR__.'/../'.$path;
         return true;
