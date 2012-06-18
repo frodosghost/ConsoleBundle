b6941dfdac06292426ddc5d5c054edb36b816dd3
James Rickard <james@frodosghost.com>
2012-06-18 10:29:12 +1000

    Updated menu building event to allow bundles to insert enu items.
    Added Configure event and base Menu Events.
    Updated menu builder to insert event dispatcher and security component.
    Updated menu configuration to inject additional classes.

---
+++ MenuEvents.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * Configure menu events for building the console menu from other bundles
+ *
+ * @author James Rickard <james@frodosghost.com>
+ */
+
+namespace Manhattan\Bundle\ConsoleBundle;
+
+final class MenuEvents
+{
+    const CONFIGURE = 'manhattan_console_bundle.menu_configure';
+}
