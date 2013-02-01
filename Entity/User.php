076d7fdd78a7f1af92bbd8394d9cb932993b4ce5
James Rickard <james@frodosghost.com>
2013-02-01 11:35:08 +1100

    Updated the User class setup. Using yml for config. Updated Entity and added test.

--- Entity/User.php
+++ Entity/User.php
@@ -6,22 +6,102 @@ use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
- * @ORM\Table(name="manhattan_user")
+ * User
  */
 class User extends BaseUser
 {
     /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var integer
      */
     protected $id;
+
+    /**
+     * @var \DateTime
+     */
+    private $created_at;
+
+    /**
+     * @var \DateTime
+     */
+    private $updated_at;
+
 
     public function __construct()
     {
         parent::__construct();
-        // your own logic
     }
-}
 
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set created_at
+     *
+     * @param \DateTime $createdAt
+     * @return User
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->created_at = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set updated_at
+     *
+     * @param \DateTime $updatedAt
+     * @return User
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updated_at = $updatedAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get updated_at
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function onCreate()
+    {
+        $this->setCreatedAt(new \DateTime());
+        $this->setUpdatedAt(new \DateTime());
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onUpdate()
+    {
+        $this->setUpdatedAt(new \DateTime());
+    }
+
+}
---
+++ Tests/Entity/UserTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Manhattan\Bundle\ConsoleBundle\Tests\Entity;
+
+use Manhattan\Bundle\ConsoleBundle\Entity\User;
+
+/**
+ * UserTest
+ *
+ * @author James Rickard <james@frodosghost.com>
+ */
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test onCreate Behaviour
+     */
+    public function testOnCreate()
+    {
+        $user = new User();
+
+        $this->assertNull($user->getUpdatedAt(), '->getUpdatedAt() returns NULL is the onCreate has not been called.');
+        $this->assertNull($user->getCreatedAt(), '->getCreatedAt() returns NULL is the onCreate has not been called.');
+
+        $user->onCreate();
+
+        $this->assertInstanceOf('\DateTime', $user->getUpdatedAt(), '->getUpdatedAt() returns DateTime instance when onCreate is called.');
+        $this->assertInstanceOf('\DateTime', $user->getCreatedAt(), '->getCreatedAt() returns DateTime instance when onCreate is called.');
+    }
+
+    /**
+     * Test onUpdate Behaviour
+     */
+    public function testOnUpdate()
+    {
+        $user = new User();
+
+        $this->assertNull($user->getUpdatedAt(), '->getUpdatedAt() returns NULL when onUpdate has not been called.');
+        $this->assertNull($user->getCreatedAt(), '->getCreatedAt() returns NULL when onUpdate has not been called.');
+
+        $user->onUpdate();
+
+        $this->assertInstanceOf('\DateTime', $user->getUpdatedAt(), '->getUpdatedAt() returns DateTime instance when onUpdate is called.');
+        $this->assertNull($user->getCreatedAt(), '->getCreatedAt() has not been updated when onUpdate is called.');
+    }
+
+}
