99a68aac1a2d4b8f94317a1f72ef9f47d3e1a21c
James Rickard <james@frodosghost.com>
2013-05-24 12:21:06 +1000

    Added prepend to easily extend the TwigBundle form resource settings.

--- DependencyInjection/ManhattanConsoleExtension.php
+++ DependencyInjection/ManhattanConsoleExtension.php
@@ -2,17 +2,18 @@
 
 namespace Manhattan\Bundle\ConsoleBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ManhattanConsoleExtension extends Extension
+class ManhattanConsoleExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -34,4 +35,32 @@ class ManhattanConsoleExtension extends Extension
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
+
+    /**
+     * Prepend form field settings for TwigBundle
+     *
+     * {@inheritDoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        // determine if TwigBundle is registered
+        if (isset($bundles['TwigBundle'])) {
+
+            $config = array('form' => array('resources' => array (
+                'ManhattanConsoleBundle:Form:fields.html.twig',
+                'OrnjMarkdownBundle:Form:fields.html.twig'
+            )));
+
+            foreach ($container->getExtensions() as $name => $extension) {
+                switch ($name) {
+                case 'twig':
+                    $container->prependExtensionConfig($name, $config);
+                    break;
+                }
+            }
+        }
+    }
+
 }
