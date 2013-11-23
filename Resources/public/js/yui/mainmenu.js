e27488de9001f1c10699ba6bd19231630c8a930a
James Rickard <james@frodosghost.com>
2013-11-23 12:02:32 +1100

    Added YUI javascript for beginnings of menu. The Menu styles are not completed with javascript enables.

---
+++ Resources/public/js/yui/mainmenu.js
@@ -0,0 +1,21 @@
+/*
+  -- Menu Hiding
+
+YUI({
+    classNamePrefix: 'pure'
+}).use('gallery-sm-menu', function (Y) {
+
+    var horizontalMenu = new Y.Menu({
+        container         : '#main-menu',
+        sourceNode        : '#nav-menu',
+        orientation       : 'vertical',
+        hideOnOutsideClick: false,
+        hideOnClick       : false
+    });
+
+    horizontalMenu.render();
+    horizontalMenu.show();
+
+});
+
+ */
