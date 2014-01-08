feee2407599a0e25f7111f04f708a7f3207dc183
James Rickard <james@frodosghost.com>
2014-01-08 15:55:20 +1100

    Updated alert and layout styles.

--- Resources/public/js/yui/mainmenu.js
+++ Resources/public/js/yui/mainmenu.js
@@ -1,21 +1,36 @@
-/*
-  -- Menu Hiding
+/*(function (window, document) {
 
-YUI({
-    classNamePrefix: 'pure'
-}).use('gallery-sm-menu', function (Y) {
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink');
 
-    var horizontalMenu = new Y.Menu({
-        container         : '#main-menu',
-        sourceNode        : '#nav-menu',
-        orientation       : 'vertical',
-        hideOnOutsideClick: false,
-        hideOnClick       : false
-    });
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
 
-    horizontalMenu.render();
-    horizontalMenu.show();
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
 
-});
+        element.className = classes.join(' ');
+    }
 
- */
+    menuLink.onclick = function (e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+    };
+
+}(this, this.document));
+*/
