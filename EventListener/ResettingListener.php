8e1632bcd48d23991ec29a0b7dcbf4d4c4cb3948
James Rickard <james@frodosghost.com>
2014-03-19 11:47:59 +1100

    Added Resetting Listener for adding subdomain to resetting links.
    Updated config to remove some of the settings now that the FOS Userbundle must be loaded earlier in AppKernel.
    Added service definitions to setup.
    Updated button translations reference in passwordset template.

---
+++ EventListener/ResettingListener.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Manhattan Console Bundle
+ *
+ * (c) James Rickard <james@frodosghost.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Manhattan\Bundle\ConsoleBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use Manhattan\Bundle\ConsoleBundle\Site\SiteManager;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class ResettingListener implements EventSubscriberInterface
+{
+    private $router;
+    private $tokenTtl;
+    private $siteManager;
+
+    public function __construct(UrlGeneratorInterface $router, SiteManager $siteManager, $tokenTtl)
+    {
+        $this->router = $router;
+        $this->siteManager = $siteManager;
+        $this->tokenTtl = $tokenTtl;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::RESETTING_RESET_INITIALIZE => 'onResettingResetInitialize',
+            FOSUserEvents::RESETTING_RESET_SUCCESS => 'onResettingResetSuccess'
+        );
+    }
+
+    public function onResettingResetInitialize(GetResponseUserEvent $event)
+    {
+        if (!$event->getUser()->isPasswordRequestNonExpired($this->tokenTtl)) {
+            $event->setResponse(new RedirectResponse($this->router->generate('fos_user_resetting_request', array(
+                'subdomain' => $this->siteManager->getSubdomain()
+            ))));
+        }
+    }
+
+    public function onResettingResetSuccess(FormEvent $event)
+    {
+        /** @var $user \FOS\UserBundle\Model\UserInterface */
+        $user = $event->getForm()->getData();
+
+        $user->setConfirmationToken(null);
+        $user->setPasswordRequestedAt(null);
+        $user->setEnabled(true);
+    }
+}
