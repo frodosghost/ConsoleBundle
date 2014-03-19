ca44bf9546bb55d988779637481806c0b60a5ceb
James Rickard <james@frodosghost.com>
2014-03-19 12:16:30 +1100

    Added Compiler Pass to setup the Access Control section of the secuirty file by automatically adding path, configuration and roles to the bundles base definition. Removed comment from services.

--- ManhattanConsoleBundle.php
+++ ManhattanConsoleBundle.php
@@ -1,8 +1,19 @@
 <?php
 
+/*
+ * This file is part of the Manhattan Console Bundle
+ *
+ * (c) James Rickard <james@frodosghost.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Manhattan\Bundle\ConsoleBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Manhattan\Bundle\ConsoleBundle\DependencyInjection\Compiler\AccessControlPass;
 
 class ManhattanConsoleBundle extends Bundle
 {
@@ -10,4 +21,11 @@ class ManhattanConsoleBundle extends Bundle
 	{
 		return 'FOSUserBundle';
 	}
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new AccessControlPass());
+    }
 }
