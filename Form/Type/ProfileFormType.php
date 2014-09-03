7bc84d98d2d3b6c6cfda44264e4e308e064d27e4
James Rickard <james@frodosghost.com>
2014-09-03 15:25:07 +1000

    Added profile form type, which can then have specific fields attached for Users. Updated configuration to include Mopa Bootstrap config settings. Added profile form to services. Updated routing to include the Subdomain.

---
+++ Form/Type/ProfileFormType.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Manhattan Console Bundle
+ *
+ * (c) James Rickard <james@frodosghost.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Manhattan\Bundle\ConsoleBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProfileFormType extends AbstractType
+{
+    private $class;
+
+    /**
+     * @param string $class The User class name
+     */
+    public function __construct($class)
+    {
+        $this->class = $class;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            /*->add('firstname', null, array(
+                'label' => 'form.firstname',
+                'translation_domain' => 'FOSUserBundle'
+            ))
+            ->add('lastname', null, array(
+                'label' => 'form.lastname',
+                'translation_domain' => 'FOSUserBundle'
+            ))*/
+            ->add('username', null, array(
+                'label' => 'form.username',
+                'translation_domain' => 'FOSUserBundle'
+            ))
+            ->add('email', 'email', array(
+                'label' => 'form.email',
+                'translation_domain' => 'FOSUserBundle'
+            ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => $this->class,
+            'intention'  => 'profile',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'fos_user_profile';
+    }
+
+}
