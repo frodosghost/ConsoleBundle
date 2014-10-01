4c8c429d0a43721d8bbe398b93229d691b18dbe6
James Rickard <james@frodosghost.com>
2014-10-02 00:01:24 +1000

    Updated twig mailer to remove base bundle setup. Updated services to inject site manager. Added resetting controller to override the redirecting URL to include the subdomain in route.

---
+++ Controller/ResettingController.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Manhattan Console Bundle
+ *
+ * (c) James Rickard <james@frodosghost.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Manhattan\Bundle\ConsoleBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+use FOS\UserBundle\Controller\ResettingController as BaseController;
+
+/**
+ * Controller managing the resetting of the password
+ *
+ * @author Thibault Duplessis <thibault.duplessis@gmail.com>
+ * @author Christophe Coevoet <stof@notk.org>
+ */
+class ResettingController extends BaseController
+{
+
+    /**
+     * Request reset user password: submit form and send email
+     */
+    public function sendEmailAction(Request $request)
+    {
+        $username = $request->request->get('username');
+
+        /** @var $user UserInterface */
+        $user = $this->container->get('fos_user.user_manager')->findUserByUsernameOrEmail($username);
+
+        if (null === $user) {
+            return $this->container->get('templating')->renderResponse('FOSUserBundle:Resetting:request.html.twig', array('invalid_username' => $username));
+        }
+
+        if ($user->isPasswordRequestNonExpired($this->container->getParameter('fos_user.resetting.token_ttl'))) {
+            return $this->container->get('templating')->renderResponse('FOSUserBundle:Resetting:passwordAlreadyRequested.html.twig');
+        }
+
+        if (null === $user->getConfirmationToken()) {
+            /** @var $tokenGenerator \FOS\UserBundle\Util\TokenGeneratorInterface */
+            $tokenGenerator = $this->container->get('fos_user.util.token_generator');
+            $user->setConfirmationToken($tokenGenerator->generateToken());
+        }
+
+        $this->container->get('fos_user.mailer')->sendResettingEmailMessage($user);
+        $user->setPasswordRequestedAt(new \DateTime());
+        $this->container->get('fos_user.user_manager')->updateUser($user);
+
+        return new RedirectResponse($this->container->get('router')->generate('fos_user_resetting_check_email',
+            array(
+                'email' => $this->getObfuscatedEmail($user),
+                'subdomain' => $this->container->get('manhattan.console.site')->getSubdomain()
+            )
+        ));
+    }
+
+}
--- Mailer/TwigSwiftMailer.php
+++ Mailer/TwigSwiftMailer.php
@@ -13,16 +13,62 @@ namespace Manhattan\Bundle\ConsoleBundle\Mailer;
 
 use FOS\UserBundle\Model\UserInterface;
 use FOS\UserBundle\Mailer\MailerInterface;
+use Manhattan\Bundle\ConsoleBundle\Site\SiteManager;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use FOS\UserBundle\Mailer\TwigSwiftMailer as BaseTwigSwiftMailer;
 
 /**
  * TwigSwiftMailer
  *
  * Provides email functionality
  */
-class TwigSwiftMailer extends BaseTwigSwiftMailer
+class TwigSwiftMailer
 {
+    private $mailer;
+    private $router;
+    private $twig;
+    private $parameters;
+    private $siteManager;
+
+    public function __construct(\Swift_Mailer $mailer, UrlGeneratorInterface $router, \Twig_Environment $twig, SiteManager $siteManager, array $parameters)
+    {
+        $this->mailer = $mailer;
+        $this->router = $router;
+        $this->twig = $twig;
+        $this->parameters = $parameters;
+        $this->siteManager = $siteManager;
+    }
+
+    public function sendConfirmationEmailMessage(UserInterface $user)
+    {
+        $template = $this->parameters['template']['confirmation'];
+        $url = $this->router->generate('fos_user_registration_confirm', array(
+            'token' => $user->getConfirmationToken(),
+            'subdomain' => $this->siteManager->getSubdomain()
+        ), true);
+
+        $context = array(
+            'user' => $user,
+            'confirmationUrl' => $url
+        );
+
+        $this->sendMessage($template, $context, $this->parameters['from_email']['confirmation'], $user->getEmail());
+    }
+
+    public function sendResettingEmailMessage(UserInterface $user)
+    {
+        $template = $this->parameters['template']['resetting'];
+        $url = $this->router->generate('fos_user_resetting_reset', array(
+            'token' => $user->getConfirmationToken(),
+            'subdomain' => $this->siteManager->getSubdomain()
+        ), true);
+
+        $context = array(
+            'user' => $user,
+            'confirmationUrl' => $url
+        );
+
+        $this->sendMessage($template, $context, $this->parameters['from_email']['resetting'], $user->getEmail());
+    }
 
     /**
      * Sends an email when a User is created
@@ -47,6 +93,35 @@ class TwigSwiftMailer extends BaseTwigSwiftMailer
         );
 
         $this->sendMessage($template, $context, $this->parameters['from_email']['confirmation'], $user->getEmail());
+    }
+
+    /**
+     * @param string $templateName
+     * @param array  $context
+     * @param string $fromEmail
+     * @param string $toEmail
+     */
+    protected function sendMessage($templateName, $context, $fromEmail, $toEmail)
+    {
+        $context = $this->twig->mergeGlobals($context);
+        $template = $this->twig->loadTemplate($templateName);
+        $subject = $template->renderBlock('subject', $context);
+        $textBody = $template->renderBlock('body_text', $context);
+        $htmlBody = $template->renderBlock('body_html', $context);
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject($subject)
+            ->setFrom($fromEmail)
+            ->setTo($toEmail);
+
+        if (!empty($htmlBody)) {
+            $message->setBody($htmlBody, 'text/html')
+                ->addPart($textBody, 'text/plain');
+        } else {
+            $message->setBody($textBody);
+        }
+
+        $this->mailer->send($message);
     }
 
 }
